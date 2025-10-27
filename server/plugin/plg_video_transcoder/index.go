7ec0d2182be649fce5b8c9aeb63b24ac1d2e728f
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2025-10-27 08:48:16 +0000

    fix: Use mpegts muxer for single segment output

--- server/plugin/plg_video_transcoder/index.go
+++ server/plugin/plg_video_transcoder/index.go
@@ -209,13 +209,9 @@ func hlsTranscodeHandler(ctx *App, res http.ResponseWriter, req *http.Request) {
 			"8x8dct=0",
 			"partitions=none",
 		}, ":"),
-		"-force_key_frames", fmt.Sprintf("expr:gte(t,n_forced*%d.000)", HLS_SEGMENT_LENGTH),
-		"-f", "ssegment",
-		"-segment_time", fmt.Sprintf("%d.00", HLS_SEGMENT_LENGTH),
-		"-segment_start_number", fmt.Sprintf("%d", segmentNumber),
-		"-initial_offset", fmt.Sprintf("%d.00", startTime),
-		"-vsync", "2",
-		"pipe:out%03d.ts",
+		"-f", "mpegts",
+		"-vsync", "passthrough",
+		"pipe:1",
 	}...)
 
 	var buffer bytes.Buffer
